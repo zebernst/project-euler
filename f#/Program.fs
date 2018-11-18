b513e9247d0a94e2deea619020d51945489ecb02
Zach Bernstein <zebernst@gmail.com>
2018-11-18 12:59:45 -0500

    solve problem 2

--- f#/Program.fs
+++ f#/Program.fs
@@ -6,6 +6,6 @@ let main argv =
     printfn "%A" argv
     
     
-    let sum = p1.MultipleSum 1000
+    let sum = p2.evenFibSum
     printfn "%A" sum
     0 // return an integer exit code
--- f#/problems/p2.fs
+++ f#/problems/p2.fs
@@ -1,2 +1,8 @@
 module Project_Euler.problems.p2
 
+let fibonacci = Seq.unfold(fun (a, b) -> Some(a+b, (b, a+b))) (1, 1)
+let evenFibSum = 
+    fibonacci 
+    |> Seq.takeWhile (fun x -> x <= 4000000)
+    |> Seq.where (fun x -> x % 2 = 0)
+    |> Seq.sum
